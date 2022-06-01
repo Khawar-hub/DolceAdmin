7e7e3e0defbdf2fa7a2d8b28ea1faf5b5e7006e2
Develo It Solution <khawarqureshi420@yahoo.com>
2022-06-01 10:48:39 +0500

    push

---
+++ src/Components/AddUser/AddUser.jsx
@@ -0,0 +1,323 @@
+import React, { useState, useEffect } from "react";
+import { firebase } from "../../Firebase/config";
+
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Grid,
+  Button,
+  Box,
+  LinearProgress,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  Typography,
+  Chip,
+} from "@mui/material";
+import { Formik, Form, Field } from "formik";
+import { TextField } from "formik-mui";
+import * as yup from "yup";
+import { useSnackbar } from "notistack";
+import { ADD_AUTH_USER } from "../../Shared/baseURL";
+
+const ref = firebase.firestore().collection("Managers");
+
+const AddUser = ({
+  open,
+  getUsers,
+  handleClose,
+  editUser,
+  edit,
+}) => {
+  // state
+  const { enqueueSnackbar: notify } = useSnackbar();
+  const [preference, setPreference] = useState([]);
+
+  // validation schema
+  const AddSchema = yup.object().shape({
+    name: yup.string().required("Required"),
+    phone: yup.string(),
+    email: yup.string().email().required("Required"),
+    username: yup.string().required("Required"),
+    Age: yup.number().min(1).positive().integer().required("Required"),
+    Gender: yup.string().required("Required"),
+    password: edit
+      ? yup.string()
+      : yup
+          .string()
+          .required("Required")
+          .min(6, "Password must be greater than 6 characters"),
+  });
+
+  // initial states
+  const initialState = {
+    name: "",
+    email: "",
+    username: "",
+    Age: "",
+    Gender: "Male",
+    phone: "",
+    password: "",
+  
+  };
+
+  const editInitialState = {
+    name: editUser?.name,
+    email: editUser?.email,
+    username: editUser?.username,
+    Age: editUser?.Age,
+    Gender: editUser?.Gender,
+    phone: editUser?.phone ?? "",
+  };
+
+  // useEffect(() => {
+  //   if (edit && editUser) {
+  //     if (Array.isArray(editUser.Preferences)) {
+  //       let arr = [...editUser.Preferences];
+  //       setPreference(arr);
+  //     }
+  //   }
+  // }, [edit, editUser]);
+
+  const handleSubmit = async (values, setSubmitting) => {
+    try {
+      // const res = await axios.post(ADD_AUTH_USER, {
+      //   email: values.email,
+      //   password: values.password,
+      // });
+      // if (res.status === 200) {
+      //   console.log(res.data);
+      //   const { userID } = res.data;
+      const _id=firebase.firestore().collection('Random').doc().id;
+        let data = {
+          ...values,
+          id: _id,
+          
+        };
+      //   delete data.password;
+    
+        await ref
+          .doc(_id)
+          .set(data, { merge: true })
+          .then(() => {
+            notify("Manager added");
+            getUsers();
+          });
+      
+    } catch (error) {
+      if (error.response) {
+        notify(error.response.data, { variant: "error" });
+      }
+    } finally {
+      setSubmitting(false);
+      restoreInitialState();
+    }
+  };
+
+  // const handleAddPreferences = (interest) => {
+  //   let arr = [...preference];
+  //   if (arr.includes(interest)) {
+  //     arr = arr.filter((item) => item !== interest);
+  //   } else {
+  //     arr.push(interest);
+  //   }
+  //   setPreference(arr);
+  // };
+
+  const handleEditSubmit = async (values, setSubmitting) => {
+    try {
+      // let data = {
+      //   ...values,
+      //   Preferences: preference,
+      // };
+      // console.log(data);
+      await ref
+        .doc(editUser.id)
+        .set(values, { merge: true })
+        .then(() => {
+          notify(`${editUser.name} updated.`);
+          getUsers();
+        });
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setSubmitting(false);
+      restoreInitialState();
+    }
+  };
+
+  const restoreInitialState = () => {
+    handleClose();
+    // setPreference([]);
+  };
+
+  return (
+    <Dialog maxWidth="md" fullWidth open={open} onClose={handleClose}>
+      <DialogTitle>
+        {edit ? `Edit ${editUser.name}` : "Add Manager"}
+      </DialogTitle>
+      <DialogContent>
+        <Formik
+          initialValues={edit ? editInitialState : initialState}
+          onSubmit={(values, { setSubmitting }) => {
+            if (edit) {
+              handleEditSubmit(values, setSubmitting);
+            } else {
+              handleSubmit(values, setSubmitting);
+            }
+          }}
+          validationSchema={AddSchema}
+        >
+          {({ isSubmitting, submitForm, values, setFieldValue }) => (
+            <Form>
+              <Grid container sx={{ mt: 2 }} spacing={2}>
+                <Grid item xs={12} md={6}>
+                  <Field
+                    component={TextField}
+                    label="Name"
+                    name="name"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <Field
+                    component={TextField}
+                    label="Username"
+                    name="username"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <Field
+                    component={TextField}
+                    label="Email"
+                    name="email"
+                    fullWidth
+                  />
+                </Grid>
+                {!edit && (
+                  <Grid item xs={12} md={6}>
+                    <Field
+                      component={TextField}
+                      label="Password"
+                      name="password"
+                      fullWidth
+                    />
+                  </Grid>
+                )}
+                <Grid item xs={12} md={6}>
+                  <Field
+                    component={TextField}
+                    label="Age"
+                    name="Age"
+                    type="text"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <Field
+                    component={TextField}
+                    label="Phone"
+                    name="phone"
+                    fullWidth
+                  />
+                </Grid>
+                {/* {!edit && (
+                  <Grid item xs={12} md={6}>
+                    <FormControl>
+                      <FormLabel id="demo-row-radio-buttons-group-label">
+                        User Type
+                      </FormLabel>
+                      <RadioGroup
+                        row
+                        onChange={(e) => {
+                          setFieldValue(
+                            "profileStatus",
+                            parseInt(e.target.value)
+                          );
+                        }}
+                      >
+                        <FormControlLabel
+                          value={0}
+                          control={<Radio />}
+                          label="Standard"
+                          checked={values.profileStatus === 0}
+                        />
+                        <FormControlLabel
+                          value={1}
+                          control={<Radio />}
+                          label="Business"
+                          checked={values.profileStatus === 1}
+                        />
+                      </RadioGroup>
+                    </FormControl>
+                  </Grid>
+                )} */}
+                <Grid item xs={12} md={6}>
+                  <FormControl>
+                    <FormLabel id="demo-row-radio-buttons-group-label">
+                      Gender
+                    </FormLabel>
+                    <RadioGroup
+                      row
+                      onChange={(e) => setFieldValue("Gender", e.target.value)}
+                    >
+                      <FormControlLabel
+                        value="Male"
+                        control={<Radio />}
+                        label="Male"
+                        checked={values.Gender === "Male"}
+                      />
+                      <FormControlLabel
+                        value="Female"
+                        control={<Radio />}
+                        label="Female"
+                        checked={values.Gender === "Female"}
+                      />
+                      <FormControlLabel
+                        value="Other"
+                        control={<Radio />}
+                        label="Other"
+                        checked={values.Gender === "Other"}
+                      />
+                    </RadioGroup>
+                  </FormControl>
+                </Grid>
+              
+                <Grid item xs={12}>
+                  {isSubmitting && <LinearProgress />}
+                </Grid>
+                <Grid item xs={12}>
+                  <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                    <Button
+                      onClick={restoreInitialState}
+                      variant="outlined"
+                      color="error"
+                      sx={{ mr: 1 }}
+                      disabled={isSubmitting}
+                    >
+                      Close
+                    </Button>
+                    <Button
+                      disabled={isSubmitting}
+                      disableElevation
+                      onClick={submitForm}
+                      variant="contained"
+                    >
+                      Submit
+                    </Button>
+                  </Box>
+                </Grid>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddUser;
--- src/Pages/Users/Users.jsx
+++ src/Pages/Users/Users.jsx
@@ -1,22 +1,289 @@
-import React, { useEffect } from "react";
-import { Divider, Typography, Grid } from "@mui/material";
+import React,{useState,useEffect,lazy} from 'react'
+import {
+    Divider,
+    Typography,
+    Card,
+    CardActionArea,
+    Paper,
+    Table,
+    IconButton,
+    Box,
+    Button,
+    Grid,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+    TextField,
+  
+    ButtonGroup,
+    CircularProgress,
+    Collapse,
+    Radio,
+    FormControl,
+    FormControlLabel,
+    RadioGroup,
+    FormLabel,
+    Chip,
+    
+    TablePagination,
+  } from "@mui/material";
+  import { cloneDeep } from "lodash";
+  import { firebase } from "../../Firebase/config";
+  import { useSnackbar } from "notistack";
+  import dayjs from "dayjs";
 
-const Users = () => {
-  useEffect(() => {}, []);
+  import { KeyboardArrowRight, KeyboardArrowDown } from "@mui/icons-material";
+  const AddUser = lazy(() =>
+  import("../../Components/AddUser/AddUser")
+);
 
-  return (
-    <div>
-      <Grid container columnSpacing={2} rowSpacing={2}>
-        <Grid item xs={12}>
-          <Divider textAlign="left">
-            <Typography variant="h5" color="primary">
-              Users
-            </Typography>
-          </Divider>
-        </Grid>
+const ref = firebase.firestore().collection("Managers");
+  const Stats = () => {
+    const { enqueueSnackbar: notify } = useSnackbar();
+      const [search, setSearch] = useState([]);
+    const [allFilteredData, setAllFilteredData] = useState([]);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+    const [addUserDialog, setAddUserDialog] = useState(false);
+    const [editUser, setEditUser] = useState(null);
+    const [edit, setEdit] = useState(false);
+    const[managers,setManagers]=useState([])
+    const [searchValue, setSearchValue] = useState("");
+    useEffect(() => {
+      let arr = handleSearch(searchValue);
+      setSearch(arr);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [allFilteredData]);
+    useEffect(()=>{
+       getUsers()
+    
+    },[managers])
+    const handleUserDialogClose = () => {
+      setAddUserDialog(false);
+      setEdit(false);
+      setEditUser(null);
+    };
+    const getUsers=async()=>{
+      try {
+        const allDocs = await ref.get();
+        let arr = [];
+        allDocs.forEach((doc) => arr.push({ ...doc.data(), _id: doc.id }));
+        setManagers(arr)
+        setAllFilteredData(arr)
+       
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    const handleDelete = async (id, name = "") => {
+     
+      try {
+       
+          await ref
+            .doc(id)
+            .delete()
+            .then(() => {
+              notify(`${name} deleted.`);
+              getUsers();
+            });
+          
+      } catch (error) {
+        notify(error.message, { variant: "error" });
+        console.log(error.message);
+      } finally {
+        
+      }
+    };
+    const handleSearch = (value = "") => {
+      if (value !== "") {
+        setPage(0);
+      }
+      value = value.trim().toLowerCase();
+      let arr = cloneDeep(allFilteredData);
+      arr = arr.filter(
+        (item) =>
+          item?.name?.toLowerCase().includes(value) ||
+          item?.email?.toLowerCase().includes(value)
+      );
+      setSearch(arr);
+      setSearchValue(value);
+      return arr;
+    };
+    const handlePageChange = (newPage) => {
+      setPage(newPage);
+    };
+  
+    const handleChangeRowsPerPage = (event) => {
+      setRowsPerPage(parseInt(event.target.value, 10));
+      setPage(0);
+    };
+  
+  
+    return (
+      <Box className="stats">
+        <Grid container alignItems="stretch" columnSpacing={2} rowSpacing={2}>
+          
+          <Grid item xs={12}>
+            <Divider textAlign="left">
+              <Typography variant="h5" color="primary">
+                Users
+              </Typography>
+            </Divider>
+          </Grid>
+          <Grid item xs={12}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: {
+              xs: "column",
+              sm: "row",
+            },
+            justifyContent: { xs: "start", sm: "space-between" },
+            alignItems: { xs: "start", sm: ' "center"' },
+          }}
+        >
+          <TextField
+            size="small"
+            label="Search by name or email"
+            type="text"
+            onChange={(e) => handleSearch(e.target.value)}
+          
+          />
+          <Box sx={{ mt: { xs: 2, sm: 0 } }}>
+            {/* <Button
+             
+              startIcon={
+               <KeyboardArrowRight />
+              }
+            >
+              Filters
+            </Button> */}
+            <Button
+            onClick={()=>setAddUserDialog(true)}
+              variant="contained"
+              color="primary"
+            >
+              Add User
+            </Button>
+          </Box>
+        </Box>
       </Grid>
-    </div>
-  );
-};
 
-export default Users;
+          <Grid item xs={12}>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell />
+                  <TableCell>Name</TableCell>
+                  <TableCell>Email</TableCell>
+                  <TableCell>Phone</TableCell>
+                  <TableCell>Gender</TableCell>
+                  <TableCell>Age</TableCell>
+                  <TableCell>Username</TableCell>
+                  <TableCell>Password</TableCell>
+                  <TableCell>Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+              {(rowsPerPage > 0
+                  ? search.slice(
+                      page * rowsPerPage,
+                      page * rowsPerPage + rowsPerPage
+                    )
+                  : search
+                ).map((user) => (
+                  <>
+                    <TableRow
+                      hover
+                      key={user._id}
+                      sx={{ "& > *": { borderBottom: "unset" } }}
+                    >
+                      <TableCell>
+                        <IconButton>
+                         
+                            <KeyboardArrowRight />
+                          
+                        </IconButton>
+                      </TableCell>
+                      <TableCell>
+                        {/* <Box sx={{ display: "flex", alignItems: "center" }}> */}
+                        {/* <Avatar src={user.profilePic} sx={{ mr: 1 }} /> */}
+                        {user.name}
+                        {/* </Box> */}
+                      </TableCell>
+                      <TableCell>{user.email}</TableCell>
+                      <TableCell>{user.phone}</TableCell>
+                      <TableCell>{user.Gender}</TableCell>
+                      <TableCell>{user.Age}</TableCell>
+                      <TableCell>{user.username}</TableCell>
+                     
+                      <TableCell>{user.password}</TableCell>
+                     
+                      <TableCell>
+                        
+                          <ButtonGroup size="small" variant="outlined">
+                            <Button
+                              color="info"
+                             onClick={()=>{
+                               setEditUser(user)
+                               setEdit(true)
+                               setAddUserDialog(true)
+
+                             }}
+                            >
+                              Edit
+                            </Button>
+                           
+                            <Button
+                             onClick={()=>handleDelete(user.id,user.name)}
+                              color="error"
+                            >
+                              Delete
+                            </Button>
+                          </ButtonGroup>
+                        
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell
+                        colSpan={12}
+                        sx={{ paddingBottom: 0, paddingTop: 0 }}
+                      >
+                        <Collapse unmountOnExit>
+                          {/* <UserDetails user={user} /> */}
+                        </Collapse>
+                      </TableCell>
+                    </TableRow>
+                  </>
+                ))}
+              </TableBody>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                count={allFilteredData.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={(event, newPage) => handlePageChange(newPage)}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                showFirstButton
+                showLastButton
+              />
+            </Table>
+          </TableContainer>
+          </Grid>
+        </Grid>
+        <AddUser
+        open={addUserDialog}
+        handleClose={handleUserDialogClose}
+        getUsers={getUsers}
+        editUser={editUser}
+        edit={edit}
+      />
+      </Box>
+    );
+  };
+  
+  export default Stats;
+  
